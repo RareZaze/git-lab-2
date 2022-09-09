94224d797f9dd9f17f51b5425ca933c4f9941b6d
Zachary <zl877220@ohio.edu>
2022-09-09 14:02:29 -0400

    Little code

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zachary Long
+ *        Date: 9/9/22
+ * Description: Simple code for lab!
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    //add code
+    cout << "Hello Git!!" << endl;
+    return 0;
+}//main
